26edc43428e5730c6b5be41f01cb2f03ef04e7d2
benjamin-abebe <benjamin.abebe@mths.ca>
2024-11-05 11:27:18 -0500

    Made the changes 2-15

--- main.ts
+++ main.ts
@@ -5,4 +5,29 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+let rotation = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// When "A" is pressed
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    while (true){
+        loopCounter = 0
+    while (loopCounter <= 5) {
+        pause(500)
+        sprite.move(1)
+    }
+        sprite.turn(Direction.Right, 90)
+        rotation = rotation + 1
+}
+})
